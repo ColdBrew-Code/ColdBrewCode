bd379be17535f82b54fad1cb19d56a981f959846
Shanna Graves <sgravescreations@gmail.com>
2026-06-02 22:46:38 -0500

    Edits to about, Add Nyx

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -2,7 +2,9 @@ import "./Team.css";
 
 const teamMembers = [
     { name: "Shanna Graves", role: "Full Stack Developer", bio: "Shanna is a versatile full-stack developer with a creative eye for web design. Their adaptive approach means every software solution is tailor-made to fill a need.", portfolio: "https://gravessoftware.dev"},
-    { name: "Geoffrey Wortham", role: "Frontend Developer", bio: "Geoffrey is a frontend specialist who thrives on pushing the limits of what interfaces can do. His drive to explore every corner of the craft means every project is not only beautiful but built to perform.", portfolio:"#"}
+    { name: "Geoffrey Wortham", role: "Frontend Developer", bio: "Geoffrey is a frontend specialist who thrives on pushing the limits of what interfaces can do. His drive to explore every corner of the craft means every project is not only beautiful but built to perform.", portfolio:"#"},
+    { name: "Nyx Strong", role: "Backend Developer", bio: "Nyx is a backend developer who loves to solve complex problems with elegant code. Her skills shine through on every project, ensuring that every solution is functional, efficient, and built to last.", portfolio:"#"},
+
 ];
 
 function Team() {
