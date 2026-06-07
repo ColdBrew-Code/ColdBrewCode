ab6e6720446ad0383f7d3ce31d9c01fde37e0051
Wilhelmina Vanderpool <wv0996337@otc.edu>
2026-06-07 17:08:54 -0500

    Add Mina to Team, modify Team styling

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,9 +1,10 @@
 import "./Team.css";
 
 const teamMembers = [
-    { name: "Shanna Graves", role: "Full Stack Developer", bio: "Shanna is a versatile full-stack developer with a creative eye for web design. Their adaptive approach means every software solution is tailor-made to fill a need.", portfolio: "https://gravessoftware.dev"},
+    { name: "Shanna Graves", role: "Full Stack Developer", bio: "Shanna is a versatile full stack developer with a creative eye for web design. Their adaptive approach means every software solution is tailor-made to fill a need.", portfolio: "https://gravessoftware.dev"},
     { name: "Geoffrey Wortham", role: "Frontend Developer", bio: "Geoffrey is a frontend specialist who thrives on pushing the limits of what interfaces can do. His drive to explore every corner of the craft means every project is not only beautiful but built to perform.", portfolio:"#"},
     { name: "Nyx Strong", role: "Backend Developer", bio: "Nyx is a backend developer who loves to solve complex problems with elegant code. Her skills shine through on every project, ensuring that every solution is functional, efficient, and built to last.", portfolio:"#"},
+    { name: "Wilhelmina Vanderpool", role: "Full Stack Developer", bio: "Mina is a full stack developer specializing in UI/UX and accessible design. She builds practical web applications, databases, and tools with a strong emphasis on usability, maintainability, and user experience. Her goal is to create software that not only works well but is intuitive and enjoyable to use.", portfolio:"https://minasaur.com/"}
 
 ];
 
