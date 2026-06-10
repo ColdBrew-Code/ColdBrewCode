ba1da66e9ecfbdbaddee501a63249f44dcf7b158
Wilhelmina Vanderpool <wv0996337@otc.edu>
2026-06-10 18:19:00 -0500

    Update to modern router pattern

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-import { Routes, Route } from "react-router-dom"
+import { Outlet } from "react-router-dom"
 import './App.css'
-import Home from "./components/home/Home";
-import PrivacyPolicy from "./components/legal/PrivacyPolicy";
-import TermsOfService from "./components/legal/TermsOfService";
 import Header from "./components/layout/Header"
 import { HeaderActionsProvider } from "./components/layout/HeaderActionsContext"
 import { useHeaderActions } from "./components/layout/HeaderActions"
@@ -24,11 +21,7 @@ function AppShell() {
         onBrandClick={headerActions.onBrandClick}
         onNavigateToSection={headerActions.onNavigateToSection}
       />
-      <Routes>
-        <Route path="/privacy" element={<PrivacyPolicy />} />
-        <Route path="/terms" element={<TermsOfService />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Outlet />
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,34 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import Home from './components/home/Home'
+import PrivacyPolicy from './components/legal/PrivacyPolicy'
+import TermsOfService from './components/legal/TermsOfService'
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/privacy',
+        element: <PrivacyPolicy />,
+      },
+      {
+        path: '/terms',
+        element: <TermsOfService />,
+      },
+    ],
+  },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
